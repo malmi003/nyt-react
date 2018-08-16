d3b9d3e29dc02c206fb9c25485c36d14ac3381f2
Amanda Malmin <amandamalmin@gmail.com>
2018-08-15 20:37:23 -0500

    added / and :id routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
-//  this file needs to be updated!!!
-
 const router = require("express").Router();
-// const bookRoutes = require("./books");
+const articleRoutes = require("./articles");
 
-// // Book routes
-// router.use("/books", bookRoutes);
+// Book routes
+router.use("/articles", articleRoutes);
 
 module.exports = router;
